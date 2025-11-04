c429d55e632862b3752c024e5e31c0191c076691
Kaylin Chu <kaylinltj@hotmail.com>
2025-11-05 12:10:54 +1300

    Feat: base calculator done

--- client/components/Calculator.tsx
+++ client/components/Calculator.tsx
@@ -1,63 +1,54 @@
 export default function calculate() {
-  // const input = (document.querySelector('input[placeholder="$ Extra Amount"]') as HTMLInputElement)
-  //   if( input !== null){ input.addEventListener('blur', () => {
-  //   if (input.value && !input.value.startsWith('$')) {
-  //     input.value += '$';
-  //   }
-  // })};
-  const getAmount = (id: string) => {
-  const input = document.querySelector<HTMLInputElement>(`#${id}`)
-  return Number(input?.value || 0);
-}
 
+// TODO if time allows : Refactor with function to reduce repetition
+//   const getAmount = (id: string) => {
+//   const input = document.querySelector<HTMLInputElement>(`#${id}`)
+//   return Number(input?.value || 0);
+// }
+//  const salary = getAmount('salaryInput')
 
-  const salary = getAmount('salaryInput')
+// TODO: Get input values as plain numbers
+  const salary = Number((
+    document.querySelector('input[id="salaryInput"]') as HTMLInputElement
+  ).value.replace(/[^0-9.-]+/g, '')) || 0
   const selfEmployed = Number((
     document.querySelector('input[id="selfEmployedInput"]') as HTMLInputElement
-  ).value)
+  ).value.replace(/[^0-9.-]+/g, '')) || 0
   const benefit = Number((
     document.querySelector('input[id="benefitInput"]') as HTMLInputElement
-  ).value)
+  ).value.replace(/[^0-9.-]+/g, '')) || 0
   const asset = Number((
     document.querySelector('input[id="assetInput"]') as HTMLInputElement
-  ).value)
+  ).value.replace(/[^0-9.-]+/g, '')) || 0
   const overseas = Number((
     document.querySelector('input[id="overseasInput"]') as HTMLInputElement
-  ).value)
+  ).value.replace(/[^0-9.-]+/g, '')) || 0
 
+// TODO: Get add input values to get total income
   const arr = [salary, selfEmployed, benefit, asset, overseas]
-  
-  function totalIncome {
-   if () {
-    Number(arr.reduce((a, b) => {return a + b}, 0))
-   }
-   else {
-    0
-   }
-  }
+  const totalIncome = arr.reduce((a, b) => {return a + b}, 0)
   console.log('Total Income: ', totalIncome)
-  
-  alert(`Your total taxable income is $${totalIncome}`)
 
-  // // Math.pow(a,b) = a^b (a to the power of b)
-  // // Compund Interest Formula: A = P(1 + r/n)^(nt)
-  // // A = final amount, P = principal, r = annual interest rate as a decimal, n = number of times interest applied per time period, t = number of time periods elapsed
-  // // P = amount, r = rate/100, t = time
-  // const debtRepayment =
-  //   Number(amount) * Math.pow(1 + Number(rate) / 100, Number(time))
-  // const investmentGrowth =
-  //   Number(amount) * Math.pow(1 + Number(investmentRate) / 100, Number(time))
+// TODO: tax time
+// Up to $15,600	10.5%
+// $15,601 - $53,500	17.5%
+// $53,501 - $78,100	30%
+// $78,101 - $180,000	33%
+// $180,001 and over	39%
+ function calculateTax() {
+  if (totalIncome <= 15600) {
+    return totalIncome * 0.105
+  } else if (totalIncome <= 53500) {
+    return (15600 * 0.105) + ((totalIncome - 15600) * 0.175)
+  } else if (totalIncome <= 78100) {
+    return (15600 * 0.105) + ((53500 - 15600) * 0.175) + ((totalIncome - 53500) * 0.30)
+  } else if (totalIncome <= 180000) {
+    return (15600 * 0.105) + ((53500 - 15600) * 0.175) + ((78100 - 53500) * 0.30) + ((totalIncome - 78100) * 0.33)
+  } else {
+    return (15600 * 0.105) + ((53500 - 15600) * 0.175) + ((78100 - 53500) * 0.30) + ((180000 - 78100) * 0.33) + ((totalIncome - 180000) * 0.39)
+ }}
 
-  // let resultMessage = `After ${time} years, your extra ${frequency.toLowerCase()} payment would save $${(debtRepayment - Number(amount)).toFixed(2)} in compound interest .\n`
-  // resultMessage += `If you invested the same amount at an average return of ${investmentRate}%, it will grow by $${(investmentGrowth - Number(amount)).toFixed(2)}.\n\n`
-
-  // if (investmentGrowth > debtRepayment) {
-  //   resultMessage += "It's better to invest!"
-  // } else if (investmentGrowth < debtRepayment) {
-  //   resultMessage += "It's better to repay the debt!"
-  // } else {
-  //   resultMessage += 'Both options yield the same result.'
-  // }
-  // // TODO: Display the result on the page (not in alert)
-  // alert(resultMessage)
+  const resultMessage = "Your total taxable income is: $" + totalIncome + ", and the tax payable is: $" + calculateTax().toFixed(2)
+  // TODO: Display the result on the page (not in alert)
+  alert(resultMessage)
 }
